7e77ce5ae0ea4940ff6d02f60da066720d9ac531
Alex Widener <alexwidener@gmail.com>
2015-07-04 16:17:45 -0700

    Bone splitter cleanup.

--- libs/AWGeneral.py
+++ libs/AWGeneral.py
@@ -6,17 +6,18 @@ def getCurrentSelection():
     return pmc.ls(sl=1)
 
 
-def breakAttrs(node, nodeAttr):
+def breakAttrs(node, channels):
     """
     Wrote for batch breaking of attributes instead of doing them one by one.
     :param node: The node upon which to sever an attribute connection.
     :param nodeAttr: List or string. If list, operate on the node for each attr.
     """
-    if isinstance(nodeAttr, list):
-        for a in nodeAttr:
-            pmc.disconnectAttr(node + '.' + a)
-    else:
-        pmc.disconnectAttr(node + '.' + nodeAttr)
+    if not isinstance(channels, list):
+        pmc.displayError(eINCORRECTTYPE)
+        return
+
+    for c in channels:
+        pmc.disconnectAttr(node + '.' + c)
 
 def unlockAttrs(node, channels):
     """
@@ -27,6 +28,7 @@ def unlockAttrs(node, channels):
     if not isinstance(channels, list):
         pmc.displayError(eINCORRECTTYPE)
         return
+
     for c in channels:
         pmc.setAttr(str(node) + '.' + c, lock=False, keyable=True)
 
